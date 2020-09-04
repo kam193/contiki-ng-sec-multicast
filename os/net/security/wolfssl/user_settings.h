bca96f372942a43d12463eb22fc9d5ed98ab3b98
Kamil Mankowski (kam193) <kam193@wp.pl>
2020-09-04 21:13:03 +0200

    Update wolfSSL kconfig

--- os/net/security/wolfssl/user_settings.h
+++ os/net/security/wolfssl/user_settings.h
@@ -1,47 +1,43 @@
-// Additional configuration for wolfSSL
-// You can override it by putting 'user_settings.h' in your project's catalog
+/* Additional configuration for wolfSSL */
+/* This configuration is prepared for multicas secure layer */
+/* You can override it by putting 'user_settings.h' in your project's catalog */
 
 #ifndef WOLF_USER_SETTINGS_H_
 #define WOLF_USER_SETTINGS_H_
 
-// TODO: Analyze following defines, group it logically and remove unused
 #define HAVE_ECC
 #define FP_ECC
 #define HAVE_ECC_ENCRYPT
 #define HAVE_HKDF
 
-#define WOLFSSL_SP_SMALL
+#define WOLFCRYPT_ONLY
+
+#define NO_ERROR_STRINGS
+#define NO_MD5
+#define NO_SHA
+#define NO_PSK
+#define NO_RC4
+#define NO_PWDBASED
+#define NO_RSA
+#define NO_DH
+#define NO_PKCS12
+#undef WOLFSSL_PEM_TO_DER
+#undef HAVE_HASHDRBG
+
+#define CURVED25519_SMALL
 
 #define NO_DSA
 #define WC_NO_HARDEN
 
-#define WOLFSSL_UIP
 #define WOLFSSL_USER_CURRTIME
 #define NO_WOLFSSL_MEMORY
-#define RSA_LOW_MEM
 #define NO_OLD_RNGNAME
-#define SMALL_SESSION_CACHE
 #define WOLFSSL_SMALL_STACK
-
 #define SINGLE_THREADED
 #define NO_SIG_WRAPPER
-
-#define HAVE_FFDHE_2048
-#define HAVE_CHACHA
-#define HAVE_CURVE25519
-#define CURVED25519_SMALL
-#define HAVE_ONE_TIME_AUTH
-#define WOLFSSL_DH_CONST
-
-#define HAVE_ED25519
-#define HAVE_POLY1305
 #define HAVE_SHA512
 #define WOLFSSL_SHA512
-
-#define TFM_TIMING_RESISTANT
 #define ECC_TIMING_RESISTANT
-#define WC_RSA_BLINDING
-
 #define NO_WRITEV
 #define NO_DEV_RANDOM
 #define NO_FILESYSTEM
@@ -61,9 +57,9 @@
 #define CUSTOM_RAND_GENERATE random_rand
 
 #define USER_TICKS
-static inline unsigned int LowResTimer(void)
+static inline unsigned int
+LowResTimer(void)
 {
-    return clock_seconds();
+  return clock_seconds();
 }
-
 #endif
